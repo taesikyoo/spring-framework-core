e6e92b765a9c5494e2f3655c251717ee76de0d7a
taesikyoo <yrns0907@gmail.com>
2020-08-20 15:43:55 +0900

    IoC 컨테이너 4부: @Component와 컴포넌트 스캔

--- src/main/java/com/study/springcore/SpringCoreApplication.java
+++ src/main/java/com/study/springcore/SpringCoreApplication.java
@@ -1,12 +1,23 @@
 package com.study.springcore;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
 
 @SpringBootApplication
 public class SpringCoreApplication {
 
+//    public static void main(String[] args) {
+//        SpringApplication.run(SpringCoreApplication.class, args);
+//    }
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringCoreApplication.class, args);
+        new SpringApplicationBuilder()
+                .sources(SpringCoreApplication.class)
+                .initializers((ApplicationContextInitializer<GenericApplicationContext>) ctx ->
+                        ctx.registerBean(ApplicationRunner.class, () -> args1 -> System.out.println("Functional Bean Definition")))
+                .run(args);
     }
 }
