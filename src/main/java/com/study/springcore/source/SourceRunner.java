ceb193b4512e62be7fb50d3fa8646d4a8a00ea62
taesikyoo <yrns0907@gmail.com>
2020-08-21 21:58:46 +0900

    IoC 컨테이너 9부: ResourceLoader

---
+++ src/main/java/com/study/springcore/source/SourceRunner.java
@@ -0,0 +1,26 @@
+package com.study.springcore.source;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Component
+public class SourceRunner implements ApplicationRunner {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    public void run(ApplicationArguments args) throws Exception {
+        Resource resource = resourceLoader.getResource("classpath:text.txt");
+        System.out.println(resource.exists());
+        System.out.println(resource.getDescription());
+        System.out.println(Files.readString(Path.of(resource.getURI())));
+    }
+}
+
