8331b1491b6a3fa9ecd993d6c6e14f0a61302a79
taesikyoo <yrns0907@gmail.com>
2020-08-27 23:17:55 +0900

    Resource 추상화

--- src/main/java/com/study/springcore/resource/ResourceRunner.java
+++ src/main/java/com/study/springcore/resource/ResourceRunner.java
@@ -1,8 +1,9 @@
-package com.study.springcore.source;
+package com.study.springcore.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
@@ -11,13 +12,18 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 @Component
-public class SourceRunner implements ApplicationRunner {
+public class ResourceRunner implements ApplicationRunner {
 
     @Autowired
-    private ResourceLoader resourceLoader;
+    private ApplicationContext resourceLoader;
 
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println(resourceLoader.getClass());
+
         Resource resource = resourceLoader.getResource("classpath:text.txt");
+
+        System.out.println(resource.getClass());
+
         System.out.println(resource.exists());
         System.out.println(resource.getDescription());
         System.out.println(Files.readString(Path.of(resource.getURI())));
