29bfaeb9985424a14f9c12c4257aae545b4030d7
taesikyoo <yrns0907@gmail.com>
2020-08-29 00:42:42 +0900

    Validation 추상화

---
+++ src/main/java/com/study/springcore/validation/EventValidator.java
@@ -0,0 +1,18 @@
+package com.study.springcore.validation;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class EventValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Event.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "title","not empty", "");
+    }
+}
