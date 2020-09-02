e412280be808c7306aca5d567ad6b41dfa52ee11
taesikyoo <yrns0907@gmail.com>
2020-09-02 17:54:47 +0900

    데이터 바인딩 추상화: Converter와 Formatter

---
+++ src/main/java/com/study/springcore/databinding/EventFormatter.java
@@ -0,0 +1,21 @@
+package com.study.springcore.databinding;
+
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+@Component
+public class EventFormatter implements Formatter<Event> {
+
+    @Override
+    public Event parse(String s, Locale locale) throws ParseException {
+        return new Event(Integer.parseInt(s));
+    }
+
+    @Override
+    public String print(Event event, Locale locale) {
+        return event.getId().toString();
+    }
+}
--- src/test/java/com/study/springcore/databinding/EventControllerTest.java
+++ src/test/java/com/study/springcore/databinding/EventControllerTest.java
@@ -7,13 +7,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest({
+        EventFormatter.class,
+        EventController.class})
 public class EventControllerTest {
 
     @Autowired
